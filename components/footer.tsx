4ab439c5705a1e65cdeb87b510ce5729429c6abd
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2025-08-07 14:31:19 +0700

    fix: enhance footer component layout and styling; integrate Caveat font for theme display

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,23 @@
+import { GENERAL_INFO } from '@/data/general.const';
 import React from 'react';
+import { Caveat } from 'next/font/google';
+
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: ['400'],
+});
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 text-gray-600 py-6">
+    <footer className="bg-gray-200 text-gray-600 pt-4 pb-6">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex flex-col justify-center md:flex-row md:justify-between items-center text-center">
-          <div className="text-sm">
-            &copy;Gathering 2025. All rights reserved.
+          <div
+            className={`text-2xl font-bold text-[#6A00FF] ${caveat.className} mb-3`}
+          >
+            {GENERAL_INFO.theme}
           </div>
+          <div className="text-sm">&copy;Gathering 2025</div>
           <div className="text-sm">
             Made with ❤️ by the{' '}
             <span className="font-semibold">IT SIGS Team</span>
